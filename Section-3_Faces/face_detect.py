753b72cc70a57e4d8f21f36f44c57e098c389b99
Sann-Htet <sannhtet899@gmail.com>
2023-11-04 14:52:53 +0630

    detecting faces uising haar cascade xml file

--- Section-3_Faces/face_detect.py
+++ Section-3_Faces/face_detect.py
@@ -1,12 +1,20 @@
 import cv2 as cv
 import numpy as np
 
-img = cv.imread('benedict.jpg')
+img = cv.imread(r'C:\Users\sannh\OneDrive\Desktop\Opencv_tutorial\Section-3_Faces\doctorspalmer.jpg')
 cv.imshow('Benedict', img)
 
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('Gray', gray)
 
+haar_cascade = cv.CascadeClassifier(r'C:\Users\sannh\OneDrive\Desktop\Opencv_tutorial\Section-3_Faces\haar_face.xml')
 
+faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=2)
+
+print(f'Number of faces found = {len(faces_rect)}')
+
+for (x, y, w, h) in faces_rect:
+    cv.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), thickness=2)
+cv.imshow('Detected Faces', img)
 
 cv.waitKey(0)
